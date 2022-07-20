ad54e23abdebf9c5c70250a6ef0328482c2c83dd
Dipasree Poddar <dipasreep2016@email.iimcal.ac.in>
2022-07-20 12:53:22 +0530

    Working version for deployment

--- src/cadence/scripts/getTotalSupply_script.js
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -1,11 +1,11 @@
 export const getTotalSupply =
 // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
 `
-import BottomShot from 0x7b6adb682517f137;
+import StupendouSciFilm from 0xc00cdc387b36de4f;
 
 pub fun main(): UInt64 {
 
-    return BottomShot.totalSupply;
+    return StupendouSciFilm.totalSupply;
 
 }
 `
--- src/cadence/transactions/mintNFT_tx.js
+++ src/cadence/transactions/mintNFT_tx.js
@@ -1,9 +1,11 @@
 export const mintNFT = 
 `
 // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-import BottomShot from 0x7b6adb682517f137 
-// This remains the same 
+import StupendouSciFilm from 0xc00cdc387b36de4f 
+
+// Do not change these
 import NonFungibleToken from 0x631e88ae7f1d7c20
+import MetadataViews from 0x631e88ae7f1d7c20
 
 transaction(
   recipient: Address,
@@ -12,20 +14,20 @@ transaction(
   thumbnail: String,
 ) {
   prepare(signer: AuthAccount) {
-    if signer.borrow<&BottomShot.Collection>(from: BottomShot.CollectionStoragePath) != nil {
+    if signer.borrow<&StupendouSciFilm.Collection>(from: StupendouSciFilm.CollectionStoragePath) != nil {
       return
     }
 
     // Create a new empty collection
-    let collection <- BottomShot.createEmptyCollection()
+    let collection <- StupendouSciFilm.createEmptyCollection()
 
     // save it to the account
-    signer.save(<-collection, to: BottomShot.CollectionStoragePath)
+    signer.save(<-collection, to: StupendouSciFilm.CollectionStoragePath)
 
     // create a public capability for the collection
-    signer.link<&{NonFungibleToken.CollectionPublic}>(
-      BottomShot.CollectionPublicPath,
-      target: BottomShot.CollectionStoragePath
+    signer.link<&{NonFungibleToken.CollectionPublic, MetadataViews.ResolverCollection}>(
+      StupendouSciFilm.CollectionPublicPath,
+      target: StupendouSciFilm.CollectionStoragePath
     )
   }
 
@@ -33,12 +35,12 @@ transaction(
   execute {
     // Borrow the recipient's public NFT collection reference
     let receiver = getAccount(recipient)
-      .getCapability(BottomShot.CollectionPublicPath)
+      .getCapability(StupendouSciFilm.CollectionPublicPath)
       .borrow<&{NonFungibleToken.CollectionPublic}>()
       ?? panic("Could not get receiver reference to the NFT Collection")
 
     // Mint the NFT and deposit it to the recipient's collection
-    BottomShot.mintNFT(
+    StupendouSciFilm.mintNFT(
       recipient: receiver,
       name: name,
       description: description,
